e981fbefb2072917e5056ea572d636bebf65a7e3
jongbum97 <kbumk1234@naver.com>
2023-04-03 15:09:52 +0900

    Commit 1

---
+++ main.py
@@ -0,0 +1,23 @@
+##메서드
+def add_func(n1,n2):
+    res = n1 + n2
+    return res
+
+
+
+
+
+
+##전역변수
+
+num1, num2, result = 100, 200, 0
+
+
+
+
+
+
+##메인코드
+result = add_func(num1, num2)
+print(num1,'+',num2,'=',result)
+
